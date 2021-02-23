8d555a539f53639b97630988c1b3fb2f5ef9cee2
Rodrigo Estevao <rodrigoestevao@yahoo.com>
2021-02-22 21:58:08 -0300

    Fixed the redis plugin

--- zsh/.oh-my-zsh/custom/plugins/redis-docker/redis-docker.plugin.zsh
+++ zsh/.oh-my-zsh/custom/plugins/redis-docker/redis-docker.plugin.zsh
@@ -8,5 +8,5 @@
 alias rddclean='docker system prune -f --volumes'
 # alias rddstart='rddclean; docker run -d -p 6379:6379 --name redis1 --restart unless-stopped -v $HOME/.local/var/lib/redis/redis1:/data redis'
 alias rddstart='rddclean; docker run -d --rm --name redis1 -p 6379:6379 -v $HOME/.local/var/lib/redis/redis1:/data redis'
-alias rddstop='docker stop redis1 2>/dev/null; rbdclean'
+alias rddstop='docker stop redis1 2>/dev/null; rddclean'
 alias rddrestart='rddstop && sleep 1 && rddstart'
