fc1aa2beaf64148e4bcbc7d518158075adb1fcb4
Rodrigo Estevao <rodrigoestevao@yahoo.com>
2021-03-23 08:28:02 -0300

    Updated the Redis and Postgres plugins

--- zsh/.oh-my-zsh/custom/plugins/postgres-docker/postgres-docker.plugin.zsh
+++ zsh/.oh-my-zsh/custom/plugins/postgres-docker/postgres-docker.plugin.zsh
@@ -6,9 +6,9 @@
 # Adopted pgd prefix for aliases and functions
 alias pgdclean='docker system prune -f --volumes'
 if [ -e ~/.local/etc/postgresql/postgresql.conf ]; then
-    alias pgdstart='pgdclean; docker run -d --rm --name postgres1 -e POSTGRES_PASSWORD=docker -p 5432:5432 -v ~/.local/var/lib/postgresql/postgres1/data:/var/lib/postgresql/data -v ~/.local/etc/postgresql/postgresql.conf:/etc/postgresql/postgresql.conf postgres -c "config_file=/etc/postgresql/postgresql.conf"'
+    alias pgdstart='pgdclean; docker run -d --rm --name postgres1 -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v ~/.local/var/lib/postgresql/postgres1/data:/var/lib/postgresql/data -v ~/.local/etc/postgresql/postgresql.conf:/etc/postgresql/postgresql.conf postgres:alpine -c "config_file=/etc/postgresql/postgresql.conf"'
 else
-    alias pgdstart='pgdclean; docker run -d --rm --name postgres1 -e POSTGRES_PASSWORD=docker -p 5432:5432 -v ~/.local/var/lib/postgresql/postgres1/data:/var/lib/postgresql/data postgres'
+    alias pgdstart='pgdclean; docker run -d --rm --name postgres1 -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v ~/.local/var/lib/postgresql/postgres1/data:/var/lib/postgresql/data postgres:alpine'
 fi
 alias pgdstop='docker stop postgres1 2> /dev/null'
 alias pgdrestart='pgdstop && sleep 1 && pgdstart'
--- zsh/.oh-my-zsh/custom/plugins/redis-docker/redis-docker.plugin.zsh
+++ zsh/.oh-my-zsh/custom/plugins/redis-docker/redis-docker.plugin.zsh
@@ -6,7 +6,6 @@
 # Adopted rd prefix for aliases and functions
 
 alias rddclean='docker system prune -f --volumes'
-# alias rddstart='rddclean; docker run -d -p 6379:6379 --name redis1 --restart unless-stopped -v $HOME/.local/var/lib/redis/redis1:/data redis'
 alias rddstart='rddclean; docker run -d --rm --name redis1 -p 6379:6379 -v $HOME/.local/var/lib/redis/redis1:/data redis'
 alias rddstop='docker stop redis1 2>/dev/null; rddclean'
 alias rddrestart='rddstop && sleep 1 && rddstart'
